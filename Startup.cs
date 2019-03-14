fcbd64530eee2785149774000c1af7f26e8dab56
Vic Chimenti <chimenti@seattleu.edu>
2019-03-13 18:57:41 -0700

    start up

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ImageProcessor.Support;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,13 +11,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
-namespace ImageSharpAPI
+namespace ImageProcessor
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly ILoggerFactory loggerFactory;
+
+        public Startup(IConfiguration configuration, ILoggerFactory loggerFactory)
         {
+            this.loggerFactory = loggerFactory;
+
             Configuration = configuration;
         }
 
@@ -25,23 +31,41 @@ namespace ImageSharpAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var logger = loggerFactory.CreateLogger<RawRequestBodyFormatter>();
+
+            services.AddMvc(o => o.InputFormatters.Insert(0, new RawRequestBodyFormatter(logger)));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title = "CPSC 5200 Image Processor Example Service",
+                    Version = "v1",
+                    Contact = new Contact()
+                    {
+                        Name = "Michaeljon Miller",
+                        Email = "michaeljon.miller@outlook.com"
+                    },
+                    Description = "CPSC 5200 Image Processor Example Service"
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            // order is important here
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
-            app.UseHttpsRedirection();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CPSC 5200 Image Processor Example Service");
+            });
+
             app.UseMvc();
         }
     }
