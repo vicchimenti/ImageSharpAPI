41c46f6287d5bac0ac3b4bd231a41cc2a08c0f92
Vic Chimenti <chimenti@seattleu.edu>
2019-03-13 18:55:36 -0700

    update program configure logging

--- Program.cs
+++ Program.cs
@@ -19,6 +19,11 @@ namespace ImageSharpAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
                 .UseStartup<Startup>();
     }
 }
